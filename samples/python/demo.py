5b636f3b51ff90178315b6fb7e6e9d8d28f438e9
chao <ch.ch@qq.com>
2021-05-08 18:01:47 +0800

    s

--- samples/python/demo.py
+++ samples/python/demo.py
@@ -5,8 +5,12 @@ import wechat
 import json
 import time
 from wechat import WeChatManager, MessageType
+import code
+import random
 
 wechat_manager = WeChatManager(libs_path='../../libs')
+
+room_list=['12080973535@chatroom']
 
 
 # 这里测试函数回调
@@ -17,10 +21,20 @@ def on_connect(client_id):
 
 @wechat.RECV_CALLBACK(in_class=False)
 def on_recv(client_id, message_type, message_data):
-    print('[on_recv] client_id: {0}, message_type: {1}, message:{2}'.format(client_id,
-                                                                            message_type, json.dumps(message_data)))
-
-
+    if message_type == MessageType.MT_DATA_CHATROOMS_MSG:
+        for message in message_data:
+            print('[群] wxid: {0}, nickname: {1} '.format(message['wxid'],message['nickname']));
+            if message['wxid'] in room_list :
+                #wechat_manager.send_text(client_id, message['wxid'], 'abcddd')
+                wechat_manager.send_image(1, message['wxid'], 'C:\\Users\\admin\\Desktop\\图片\\新建文件夹\\1.jpg')
+                print('sleep')
+                time.sleep(random.randint(5,15))
+                
+                
+    else:
+        #print('[on_recv] client_id: {0}, message_type: {1}, message:{2}'.format(client_id,
+        #                                                                    message_type, json.dumps(message_data,ensure_ascii=False)))
+        pass
 @wechat.CLOSE_CALLBACK(in_class=False)
 def on_close(client_id):
     print('[on_close] client_id: {0}'.format(client_id))
@@ -34,14 +48,13 @@ class LoginTipBot(wechat.CallbackHandler):
         # 判断登录成功后，就向文件助手发条消息
         if message_type == MessageType.MT_USER_LOGIN:
             time.sleep(2)
-            wechat_manager.send_text(client_id, 'filehelper', '😂😂😂\uE052该消息通过wechat_pc_api项目接口发送')
-            
-            wechat_manager.send_link(client_id, 
-            'filehelper', 
-            'wechat_pc_api项目', 
-            'WeChatPc机器人项目', 
-            'https://github.com/smallevilbeast/wechat_pc_api', 
-            'https://www.showdoc.com.cn/server/api/attachment/visitfile/sign/0203e82433363e5ff9c6aa88aa9f1bbe?showdoc=.jpg)')
+            wechat_manager.send_text(client_id, 'filehelper', '😂😂😂\uE052')
+            #wechat_manager.send_link(client_id, 
+            #'filehelper', 
+            #'wechat_pc_api项目', 
+            #'WeChatPc机器人项目', 
+            #'https://github.com/smallevilbeast/wechat_pc_api', 
+            #'https://www.showdoc.com.cn/server/api/attachment/visitfile/sign/0203e82433363e5ff9c6aa88aa9f1bbe?showdoc=.jpg)')
 
 
 if __name__ == "__main__":
@@ -50,7 +63,13 @@ if __name__ == "__main__":
     # 添加回调实例对象
     wechat_manager.add_callback_handler(bot)
     wechat_manager.manager_wechat(smart=True)
-
+    wechat_manager.get_chatrooms(1)
+    code.interact(banner = "", local = locals())
+    
+    
+    
+    
+    
     # 阻塞主线程
-    while True:
-        time.sleep(0.5)
+    #while True:
+    #    time.sleep(0.5)
