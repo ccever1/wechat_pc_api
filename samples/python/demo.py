1d5d614998cb58326639551b8f5c4447ecba0fe7
chao <ch.ch@qq.com>
2021-05-12 17:49:02 +0800

    r

--- samples/python/demo.py
+++ samples/python/demo.py
@@ -7,11 +7,23 @@ import time
 from wechat import WeChatManager, MessageType
 import code
 import random
+import os
+
+#f = open('testroom.txt', 'r')
+f = open('room.txt', 'r')
+room = f.read()
+f.close()
+room_list=room.split("\n")
+root = os.getcwd()
+img1=root+'\e1.jpg'
+img2=root+'\m2.jpg'
+currnet_img=img1
 
 wechat_manager = WeChatManager(libs_path='../../libs')
-
+#wechat_manager.send_image(1, '12080973535@chatroom', 'D:\\www\\wechat_pc_api\\samples\\python\\e1.jpg')
+#wechat_manager.send_image(1, '12080973535@chatroom', 'D:\www\wechat_pc_api\samples\python\e1.jpg')
 #room_list=['12080973535@chatroom','23610797688@chatroom','12274238766@chatroom','4612215919@chatroom','790078830@chatroom','18769307766@chatroom','24993879242@chatroom','24980967795@chatroom','6657667938@chatroom','24943141364@chatroom','23354997252@chatroom','24700464724@chatroom','25432744754@chatroom','22106310328@chatroom','21880908626@chatroom']
-room_list=['12080973535@chatroom','23610797688@chatroom','12274238766@chatroom','4612215919@chatroom','790078830@chatroom','18769307766@chatroom','24993879242@chatroom','24980967795@chatroom','23354997252@chatroom','24700464724@chatroom','21880908626@chatroom']
+#room_list=['12080973535@chatroom','23610797688@chatroom','12274238766@chatroom','4612215919@chatroom','790078830@chatroom','18769307766@chatroom','24993879242@chatroom','24980967795@chatroom','23354997252@chatroom','24700464724@chatroom','21880908626@chatroom']
 
 # 这里测试函数回调
 @wechat.CONNECT_CALLBACK(in_class=False)
@@ -21,17 +33,33 @@ def on_connect(client_id):
 
 @wechat.RECV_CALLBACK(in_class=False)
 def on_recv(client_id, message_type, message_data):
+    global currnet_img
     if message_type == MessageType.MT_DATA_CHATROOMS_MSG:
         for message in message_data:
             print('[群] wxid: {0}, nickname: {1} '.format(message['wxid'],message['nickname']));
             if message['wxid'] in room_list :
                 #wechat_manager.send_text(client_id, message['wxid'], 'abcddd')
-                wechat_manager.send_image(1, message['wxid'], 'C:\\Users\\Administrator\\Desktop\\wetool\\2.jpg')
+                wechat_manager.send_image(1, message['wxid'], currnet_img)
                 print('sleep')
                 time.sleep(random.randint(10,20))
     elif message_type == MessageType.MT_RECV_MINIAPP_MSG:
-        print('[on_recv] client_id: {0}, message_type: {1}, message:{2}'.format(client_id,
-                                                                            message_type, json.dumps(message_data,ensure_ascii=False)))                
+        #print('[on_recv] client_id: {0}, message_type: {1}, message:{2}'.format(client_id,
+        #                                                                    message_type, json.dumps(message_data,ensure_ascii=False)))                
+        pass
+    elif message_type == MessageType.MT_RECV_TEXT_MSG:
+        #print('[on_recv] client_id: {0}, message_type: {1}, message:{2}'.format(client_id,
+        #                                                                    message_type, json.dumps(message_data,ensure_ascii=False)))
+        if message_data['from_wxid'] == 'Coco237248':
+            if message_data['msg'] == '1' :
+                #print('img1')
+                currnet_img=img1
+                wechat_manager.get_chatrooms(1)
+            elif message_data['msg'] == '2' :
+                #print('img2')
+                currnet_img=img2
+                wechat_manager.get_chatrooms(1)
+            else:
+                pass
     else:
         #print('[on_recv] client_id: {0}, message_type: {1}, message:{2}'.format(client_id,
         #                                                                    message_type, json.dumps(message_data,ensure_ascii=False)))
